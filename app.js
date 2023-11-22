a8af0daf22342d84dcc6d3973fdd028075a053b4
hugoty <hugo.raoult@hotmail.fr>
2023-11-22 15:15:34 +0100

    change port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const db = mongoose.connection;
 
 const cors = require("cors");
