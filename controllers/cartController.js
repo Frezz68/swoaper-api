a11f83be4b2b152ed2960a6302af057df7342e97
hugoty <hugo.raoult@hotmail.fr>
2024-03-06 14:12:36 +0100

    add service to car

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -2,6 +2,47 @@ const Cart = require("../models/cart");
 const jwt = require("jsonwebtoken");
 const { decryptJwtToken } = require("../utils/jwtUtils");
 
+// Ajouter un service au panier de l'utilisateur
+exports.addServiceToCart = async (req, res) => {
+  const serviceId = req.body.serviceId; // ID du service à ajouter, passé dans le corps de la requête
+  const token = req.headers.authorization.split(" ")[1]; // Extrait le token JWT du header Authorization
+  const decryptedToken = decryptJwtToken(token);
+
+  if (!decryptedToken) {
+    return res
+      .status(401)
+      .send("L'utilisateur n'est pas connecté ou le token est invalide.");
+  }
+
+  const userId = decryptedToken.userId; // Extrait l'ID de l'utilisateur depuis le token déchiffré
+
+  try {
+    // Trouver le panier de l'utilisateur par son ID et ajouter le service à la liste des services
+    const userCart = await Cart.findById(userId);
+
+    if (!userCart) {
+      return res.status(404).json({ message: "Cart not found" });
+    }
+
+    // Vérifier si le service est déjà dans le panier pour éviter les doublons
+    if (!userCart.services.includes(serviceId)) {
+      userCart.services.push(serviceId); // Ajouter le serviceId au panier
+      await userCart.save(); // Sauvegarder les changements dans le panier
+      res.status(200).json({
+        message: "Service added to cart successfully",
+        cart: userCart,
+      });
+    } else {
+      res.status(400).json({ message: "Service already in cart" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Error while adding service to cart",
+      error: error.message,
+    });
+  }
+};
+
 /**
  * @swagger
  * components:
@@ -38,29 +79,29 @@ const { decryptJwtToken } = require("../utils/jwtUtils");
  *               $ref: '#/components/schemas/Cart'
  */
 exports.getUserCart = async (req, res) => {
-    const token = req.headers.authorization.split(" ")[1];
-    const decryptedToken = decryptJwtToken(token);
+  const token = req.headers.authorization.split(" ")[1];
+  const decryptedToken = decryptJwtToken(token);
 
-    if (decryptedToken) {
-        try {
-            // Obtenez le panier de l'utilisateur actuellement authentifié
-            const userCart = await Cart.findById(decryptedToken.userId);
+  if (decryptedToken) {
+    try {
+      // Obtenez le panier de l'utilisateur actuellement authentifié
+      const userCart = await Cart.findById(decryptedToken.userId);
 
-            if (!userCart) {
-                return res.status(404).json({ message: "Cart not found" });
-            }
+      if (!userCart) {
+        return res.status(404).json({ message: "Cart not found" });
+      }
 
-            res.status(200).json(userCart);
-        } catch (error) {
-            res.status(500).json({
-                message: "Error while fetching user's cart",
-                error: error.message,
-            });
-        }
-    } else {
-        // Gérer l'erreur ou la situation où le token est invalide ou expiré
-        res.status(401).send("L'utilisateur n'est pas connecté.");
+      res.status(200).json(userCart);
+    } catch (error) {
+      res.status(500).json({
+        message: "Error while fetching user's cart",
+        error: error.message,
+      });
     }
+  } else {
+    // Gérer l'erreur ou la situation où le token est invalide ou expiré
+    res.status(401).send("L'utilisateur n'est pas connecté.");
+  }
 };
 
 /**
@@ -83,21 +124,21 @@ exports.getUserCart = async (req, res) => {
  *                   type: string
  */
 exports.createCart = async (req, res) => {
-    try {
-        const token = req.header("Authorization").split(" ")[1];
-        const decryptedToken = decryptJwtToken(token);
-        const userId = decryptedToken.userId;
+  try {
+    const token = req.header("Authorization").split(" ")[1];
+    const decryptedToken = decryptJwtToken(token);
+    const userId = decryptedToken.userId;
 
-        const newCart = new Cart({ _id: userId });
-        await newCart.save();
+    const newCart = new Cart({ _id: userId });
+    await newCart.save();
 
-        res.status(201).json({ message: "Cart created successfully" });
-    } catch (error) {
-        res.status(500).json({
-            message: "Error while creating the cart",
-            error: error.message,
-        });
-    }
+    res.status(201).json({ message: "Cart created successfully" });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error while creating the cart",
+      error: error.message,
+    });
+  }
 };
 
 /**
@@ -131,21 +172,21 @@ exports.createCart = async (req, res) => {
  *               $ref: '#/components/schemas/Cart'
  */
 exports.updateCart = async (req, res) => {
-    try {
-        // Remplacez le contenu du panier actuel par le nouveau contenu du corps de la demande
-        const updatedCart = await Cart.findByIdAndUpdate(
-            req.params.cartId,
-            req.body,
-            { new: true }
-        );
+  try {
+    // Remplacez le contenu du panier actuel par le nouveau contenu du corps de la demande
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.cartId,
+      req.body,
+      { new: true }
+    );
 
-        res.status(200).json(updatedCart);
-    } catch (error) {
-        res.status(500).json({
-            message: "Error while updating the cart",
-            error: error.message,
-        });
-    }
+    res.status(200).json(updatedCart);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error while updating the cart",
+      error: error.message,
+    });
+  }
 };
 
 /**
@@ -168,20 +209,20 @@ exports.updateCart = async (req, res) => {
  *         description: Successfully deleted the user's cart.
  */
 exports.deleteCart = async (req, res) => {
-    // Delete le panier toutes les semaines
-    try {
-        // Supprimez le panier de l'utilisateur par ID
-        const deletedCart = await Cart.findByIdAndRemove(req.params.cartId);
+  // Delete le panier toutes les semaines
+  try {
+    // Supprimez le panier de l'utilisateur par ID
+    const deletedCart = await Cart.findByIdAndRemove(req.params.cartId);
 
-        if (!deletedCart) {
-            return res.status(404).json({ message: "Cart not found" });
-        }
-
-        res.status(204).end();
-    } catch (error) {
-        res.status(500).json({
-            message: "Error while deleting the cart",
-            error: error.message,
-        });
+    if (!deletedCart) {
+      return res.status(404).json({ message: "Cart not found" });
     }
+
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({
+      message: "Error while deleting the cart",
+      error: error.message,
+    });
+  }
 };
--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -2,68 +2,67 @@ const Service = require("../models/service"); // Assuming Service model is in th
 
 // List all services
 exports.listServices = async (req, res) => {
-    console.log("here");
-    try {
-        const services = await Service.find();
-        console.log("services : ", services);
-        res.json(services);
-    } catch (err) {
-        res.status(500).json({
-            error: "Erreur lors de la récupération des services",
-        });
-    }
+  console.log("here");
+  try {
+    const services = await Service.find();
+    console.log("services : ", services);
+    res.json(services);
+  } catch (err) {
+    res.status(500).json({
+      error: "Erreur lors de la récupération des services",
+    });
+  }
 };
 
 // Get a service by ID
 exports.getServiceById = async (req, res) => {
-    try {
-        const service = await Service.findById(req.params.id);
-        if (!service) {
-            return res.status(404).send("Service not found");
-        }
-        res.json(service);
-    } catch (err) {
-        res.status(500).send(err.message);
+  try {
+    const service = await Service.findById(req.params.id);
+    if (!service) {
+      return res.status(404).send("Service not found");
     }
+    res.json(service);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
 // Create a new service
 exports.createService = async (req, res) => {
-    try {
-        const newService = new Service(req.body);
-        await newService.save();
-        res.status(201).json(newService);
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
+  try {
+    const newService = new Service(req.body);
+    await newService.save();
+    res.status(201).json(newService);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 };
 
 // Update a service
 exports.updateService = async (req, res) => {
-    try {
-        const service = await Service.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true, runValidators: true }
-        );
-        if (!service) {
-            return res.status(404).send("Service not found");
-        }
-        res.json(service);
-    } catch (err) {
-        res.status(400).send(err.message);
+  try {
+    const service = await Service.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!service) {
+      return res.status(404).send("Service not found");
     }
+    res.json(service);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 };
 
 // Delete a service
 exports.deleteService = async (req, res) => {
-    try {
-        const service = await Service.findByIdAndDelete(req.params.id);
-        if (!service) {
-            return res.status(404).send("Service not found");
-        }
-        res.send({ message: "Service successfully deleted" });
-    } catch (err) {
-        res.status(500).send(err.message);
+  try {
+    const service = await Service.findByIdAndDelete(req.params.id);
+    if (!service) {
+      return res.status(404).send("Service not found");
     }
+    res.send({ message: "Service successfully deleted" });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
