157c76f092677d31c55f723e5303aa51534ac5ff
Alan <alan.jordy1999@gmail.com>
2024-04-03 11:44:24 +0200

    🔧 refactor(mailController.js): remove direct email addresses for privacy and security reasons, use environment variables instead ✨ feat(mailController.js): add support for multiple file attachments in emails 🔒 fix(mailController.js): add rate limiting to prevent spamming of emails 📦 feat(package.json): add express-rate-limit and multer packages for rate limiting and file handling 🔧 refactor(mailRoutes.js): integrate multer and express-rate-limit into mailRoutes for file handling and rate limiting

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,19 +1,30 @@
+const e = require("express");
 const { Resend } = require("resend");
 
 const resend = new Resend(process.env.API_KEY_RESEND);
 
 exports.contactSendMail = async (req, res) => {
     try {
-        const { email, objet, message, pieceJointe } = req.body;
+        const { email, objet, message } = req.body;
+        const pieceJointe = req.files;
+    
         if (!email && !objet && !message) {
             return res.status(400).json({ message: "Tous les champs sont requis" });
         }
+        if(pieceJointe.length > 5){
+            return res.status(400).json({ message: "Nombre de pièces jointes limité à 5" });
+        }
         const { data, error } = await resend.emails.send({
-            from: 'no_reply@swoaper.com',
-            to: ['lucasperez.apple@gmail.com',email],
+            from: 'no_reply@contact.swoaper.com',
+            to: [process.env.TEST,email],
             subject: objet,
-            html:'<p>'+message+'</p><br>',
-            attachments: pieceJointe
+            html:'<span>Voici un recap de ce qui est envoyé :</span><br><p>'+message+'</p><br>',
+            attachments: pieceJointe.map(file => {
+                return {
+                    filename: file.originalname,
+                    content: file.buffer
+                }
+            })
         });
         if (error) {
             console.log(error);
@@ -21,6 +32,7 @@ exports.contactSendMail = async (req, res) => {
         }
         res.status(200).json(data);
     } catch (err) {
+        console.log(err);
         res.status(400).send(err.message);
     }
 };
